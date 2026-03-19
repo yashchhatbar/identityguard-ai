6c38b2bbd93d20049cba5b0e0e22d2b94068a7cc
Yash_Chhabar <yashchhatbar11@gmail.com>
2026-03-19 23:11:40 +0530

    Update vite.config.js

--- identityguard-ai/frontend/vite.config.js
+++ identityguard-ai/frontend/vite.config.js
@@ -1,13 +1,19 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
 
   server: {
     port: 5173,
-    host: "0.0.0.0", // allow external access (mobile, network)
+    host: "0.0.0.0",
+    proxy: {
+      "/api": {
+        target: "https://identityguard-ai.onrender.com",
+        changeOrigin: true,
+        secure: true,
+      },
+    },
   },
 
   preview: {
